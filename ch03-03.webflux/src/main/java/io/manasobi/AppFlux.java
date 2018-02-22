75c7c09b05130c0428302d9a4cfc774cadcbebeb
장태욱 <tw.jang.dandi@gmail.com>
2018-02-22 18:31:49 +0900

    [기능추가] 1. webfluex 예제 추가.

---
+++ ch03-03.webflux/src/main/java/io/manasobi/AppFlux.java
@@ -0,0 +1,12 @@
+package io.manasobi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppFlux {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppFlux.class, args);
+    }
+}
